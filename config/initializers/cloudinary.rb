90f3c999a5f03b2d07cbdd806d37e630b9b9e591
PaleAlex <aless.ciocchetti@gmail.com>
2021-08-10 15:53:19 +0200

    immagini in cloud

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dp5r9jn8g'
+  config.api_key = '657334945422316'
+  config.api_secret = 'y6r_VDmNxm2aepTHrLi-slQR4_U'
+  config.secure = true
+  config.cdn_subdomain = true
+end
